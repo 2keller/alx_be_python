6bab598de2b19ccb2aa00c12e1fff6ecf21403e2
2keller <mashayamombekelvin@gmail.com>
2025-07-11 11:12:31 +0200

    simple calculator

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print("addition of", number1, "and", number2, "is", sum)
+print("subtraction of ", number1, "and", number2, "is", number1 - number2)
+print("multiplication of ", number1, "and", number2, "is", number1 * number2)
