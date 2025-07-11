61eddd6de29d46e700f4a1a6a08178ec15c22355
2keller <mashayamombekelvin@gmail.com>
2025-07-11 11:26:12 +0200

    made an intrest calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000
+rate_of_interest = 0.05  # 5% expressed as a decimal
+time = 3  # in years
+simple_interest = principal * rate_of_interest * time
+print("Simple Interest is:", simple_interest)
