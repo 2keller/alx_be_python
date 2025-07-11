f9d325a7249c4e123af27e30b8f14eea1f86a142
2keller <mashayamombekelvin@gmail.com>
2025-07-11 11:29:55 +0200

    fixed the variable names for clarity

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
 principal = 1000
-rate_of_interest = 0.05  # 5% expressed as a decimal
+rate = 0.05  # 5% expressed as a decimal
 time = 3  # in years
-simple_interest = principal * rate_of_interest * time
+simple_interest = principal * rate * time
 print("Simple Interest is:", simple_interest)
