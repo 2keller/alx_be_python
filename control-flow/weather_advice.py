087ae50740a6d14a00b90df1ce0df924f5ea22ae
2keller <mashayamombekelvin@gmail.com>
2025-07-18 13:26:03 +0200

    Fix input prompt and output formatting in weather advice script

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,8 +1,8 @@
-weather = input("whats the weather like today,(sunny/rainy/cold): ")
+weather = input("Whats the weather like today:(sunny/rainy/cold) ")
 if weather == "sunny":
-    print("wear a t-shirt and sunglasses")
+    print("wear a t-shirt and sunglasses.")
 elif weather == "rainy":
-    print("Don't forget your umbrella and raincoat ")
+    print("Don't forget your umbrella and raincoat.")
 elif weather == "cold":
     print("Make sure to wear a warm coat and scarf.")
 else:
