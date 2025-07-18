18e17ab4655a03aebddb7abf26d259ac16c13410
2keller <mashayamombekelvin@gmail.com>
2025-07-18 18:41:40 +0200

    Add daily reminder script for task prioritization and time management

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,10 @@
+from difflib import Match
+
+task = input("describe the task: ")
+priority = input("choose task priority,high, medium, low: ")
+time_bound = input("is the task time bound,YES and  NO ")
+match priority | time_bound:
+    case "high" | "yes":
+        print("Reminder: finish",task ,"is a high priority task that requires immediate attention today!")
+    case "medium"| "no":
+        print(task, 'is a low priority task. Consider completing it when you have free time.')
